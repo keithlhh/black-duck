e300e56a8afa99fe11e3ab00cc5776fe0c9e36ed
lihonghui <lihonghui_lhh@163.com>
2018-12-27 01:19:58 +0800

    swiper commit

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,47 +2,23 @@ var QQMapWX = require('../../libs/qqmap-wx-jssdk.min.js');
 var qqmapsdk;
 Page({
   getLocation:function(){
-    var that=this;
-    wx.getLocation({
-      type: 'wgs84',
-      success(res) {
-        const latitude = res.latitude
-        const longitude = res.longitude
-        that.setData({
-          latitude,
-          longitude
-        });
-        console.log(that.data)
-      }
-    });
+    this.choAddr();
   },
-  /**
-   * 页面的初始数据
-   */
-  data:{
-    latitude:'',
-    longitude:'',
-    locationString:'11'
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  //初始化获取精度和纬度坐标
-  onLoad: function (options) {
+  choAddr:function(){
     var that = this;
     //promise执行异步函数
-    function getLoac(){
-      return new Promise(function(resolve,reject){
+    function getLoac() {
+      return new Promise(function (resolve, reject) {
         wx.getLocation({
-          type: 'wgs84',
+          type: 'wgs84',//gps坐标
           success(res) {
             resolve(res)
           }
         });
       })
     }
-    function changePot(){ 
-      return new Promise(function(resolve,reject){
+    function changePot() {
+      return new Promise(function (resolve, reject) {
         //将精度和维度转化为地址坐标
         var demo = new QQMapWX({
           key: '2NFBZ-A57WP-3JGDC-L3ZUL-TLMCJ-HNF7N' // 必填
@@ -52,11 +28,12 @@ Page({
             latitude: that.data.latitude,
             longitude: that.data.longitude
           },
+          coord_type: 1,//1为gps坐标
           success: function (res) {
-            console.log(res.result.address);
-            that.setData.locationString = res.result.address;
-            console.log(that.setData.locationString)
-            resolve(res);
+            that.setData({
+              locationString: res.result.formatted_addresses.recommend
+            })
+            // resolve(res);
           },
           fail: function (res) {
             //console.log(res);
@@ -67,19 +44,41 @@ Page({
         });
       })
     }
-    
+
     getLoac()
-    .then((res) => {//promise函数异步调用
-      const latitude = res.latitude
-      const longitude = res.longitude
-      that.setData({
-        latitude,
-        longitude
-      });
-      changePot();
-    }).then((res)=>{
-      console.log('then'+res);
-    })
+      .then((res) => {//promise函数异步调用
+        const latitude = res.latitude
+        const longitude = res.longitude
+        that.setData({
+          latitude,
+          longitude
+        });
+        changePot();
+        // return changePot(); //后面继续加then，需要return出去。否则不需要
+      })
+    // .then((res)=>{
+    //   console.log(res);
+    // })
+  },
+  /**
+   * 页面的初始数据
+   */
+  data:{
+    latitude:'',
+    longitude:'',
+    locationString:'',
+    imgUrls: [
+      '../../img/banner/banner1.png',
+      '../../img/banner/banner2.png',
+      '../../img/banner/banner3.png'
+    ]
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  //初始化获取精度和纬度坐标
+  onLoad: function (options) {
+    this.choAddr();
   },
 
   /**
